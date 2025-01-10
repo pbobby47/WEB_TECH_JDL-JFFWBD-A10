ed1bde2a8e21c23aacc6e616a6cc8243a71f4367
Perecharla Babi <perecharlababi47.com>
2025-01-10 21:45:10 +0530

    Youtube Project | API

--- JavaScript/Projects/Youtube Clone using API/index.js
+++ JavaScript/Projects/Youtube Clone using API/index.js
@@ -95,15 +95,17 @@ toggleButton.addEventListener("click", () => {
 
 // REFERENCE LINK: https://developers.google.com/youtube/v3/docs/search
 
-const API_KEY = "AIzaSyChL3R4VdC639_f-9RPDCSvSYVVns-PRQA";
-const SEARCH_HTTP = "https://www.googleapis.com/youtube/v3/search?"; // remember ? symbol
+// const API_KEY = "AIzaSyChL3R4VdC639_f-9RPDCSvSYVVns-PRQA"; //! DON'T USE THIS
+const API_KEY = "AIzaSyC40f8_wSV1XpxKb-h8vNDh2hQ1bIlfq4c "; //! DON'T USE THIS
+const SEARCH_HTTP = "https://www.googleapis.com/youtube/v3/search?"; // remember "?"" symbol
+const CHANNEL_HTTP = "https://www.googleapis.com/youtube/v3/channels?";
 
 let CallYoutubeDataAPI = async () => {
   let search_params = new URLSearchParams({
     key: API_KEY, // M
     part: "snippet", // M
-    q: "Milliniore", // M
-    maxResults: 3,
+    q: "JavaScript", // M
+    maxResults: 50,
     type: "video",
     regionCode: "IN",
   });
@@ -114,10 +116,75 @@ let CallYoutubeDataAPI = async () => {
 
   console.log(data.items);
 
-  console.log(data.items[0]);
-  console.log(data.items[0].id);
+  // We want thumbnail url(done) , channelIcon(pending) , title(done) , channelName(done) , video link(done)
+  data.items.map((video_data, ind) => {
+    // console.log(ind, "video---", video_data);
+    // console.log(video_data);
+    // console.log(video_data.id.videoId);
+    // console.log(`https://www.youtube.com/watch?v=${video_data.id.videoId}`);
+    // console.log(video_data.snippet.channelId);
+    // console.log(video_data.snippet.title);
+    // console.log("channel Titele ---- ", video_data.snippet.channelTitle);
+    // console.log(video_data.snippet.thumbnails.high.url);
 
-  console.log(data.items[1]);
-  console.log(data.items[2]);
+    // to get channel Icon
+    getChannelIcon(video_data);
+  });
 };
+
+// function to get channel icon
+let getChannelIcon = async video_data => {
+  let channel_params = new URLSearchParams({
+    key: API_KEY, // M
+    part: "snippet", // M,
+    id: video_data.snippet.channelId,
+  });
+
+  let res = await fetch(CHANNEL_HTTP + channel_params);
+  let data = await res.json();
+  // console.log(data);
+  // console.log(data.items);
+  // console.log(data.items[0]);
+  // console.log(data.items[0].snippet);
+  // console.log(data.items[0].snippet.thumbnails);
+  // console.log(data.items[0].snippet.thumbnails.high);
+  // console.log(data.items[0].snippet.thumbnails.high.url);
+  let channelIcon = data.items[0].snippet.thumbnails.high.url;
+
+  // DOM work starts here
+  console.log(video_data);
+  console.log(channelIcon);
+
+  appendVidesInToContainer(video_data, channelIcon);
+};
+
+// to create videos
+let appendVidesInToContainer = (video_data, channelIcon) => {
+  let main_content = document.getElementById("main_content");
+
+  main_content.innerHTML += `
+    <a href="https://www.youtube.com/watch?v=${video_data.id.videoId}">
+    
+      <main class="video_container">
+          <article class="imageBox">
+            <img src="${video_data.snippet.thumbnails.high.url}" alt="" />
+          </article>
+          <article class="infoBox">
+            <div>
+              <img src="${channelIcon}" alt="" />
+            </div>
+            <div>
+              <p>
+                ${video_data.snippet.title}
+              </p>
+              <p class="channelName">${video_data.snippet.channelTitle}</p>
+            </div>
+          </article>
+        </main>  
+
+    </a>
+    
+    `;
+};
+
 CallYoutubeDataAPI();
