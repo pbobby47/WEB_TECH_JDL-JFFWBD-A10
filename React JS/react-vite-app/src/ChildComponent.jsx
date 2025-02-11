544e7f3de2c86edd958ca4a6b7e75bd44dc40fd5
Bobby Perecharla <perecharlababi47@gmail.com>
2025-02-11 20:01:45 +0530

    Props

--- React JS/react-vite-app/src/ChildComponent.jsx
+++ React JS/react-vite-app/src/ChildComponent.jsx
@@ -53,7 +53,7 @@ export default ChildComponent;
 */
 
 // Example 2:
-
+/*
 let ChildComponent = props => {
   console.log(props);
   let {
@@ -77,4 +77,174 @@ let ChildComponent = props => {
     </>
   );
 };
+export default ChildComponent;
+*/
+
+// ? ===== Default props ====
+// 1st Way
+/*
+let ChildComponent = props => {
+  console.log(props);
+
+  let {
+    sname = "No Name",
+    age = 18,
+    course = "Generic",
+    skills = [],
+    friend = { fname: "No friends", address: "not mentioned", mobile: 0 },
+  } = props;
+
+  return (
+    <>
+      <hr />
+      <h2>I am ChildComponent</h2>
+      <h3>Name : {sname}</h3>
+      <h3>Age : {age}</h3>
+      <h3>Course : {course}</h3>
+      <h3>
+        Skills:
+        {skills.map((val, ind) => {
+          console.log(val, "----", ind);
+          return <li key={ind}>{val}</li>;
+        })}
+      </h3>
+      <h3>Friend Name: {friend.fname}</h3>
+      <h4>Mobile No : {friend.mobile}</h4>
+      <h4>Address: {friend.address}</h4>
+      <hr />
+    </>
+  );
+};
+export default ChildComponent;
+*/
+
+// 2nd way:
+/*
+let ChildComponent = props => {
+  console.log(props);
+
+  return (
+    <>
+      <hr />
+      <h2>I am ChildComponent</h2>
+      <h3>Name : {props.sname}</h3>
+      <h3>Age : {props.age}</h3>
+      <h3>Course : {props.course}</h3>
+      <h3>
+        Skills:
+        {props.skills.map((val, ind) => {
+          console.log(val, "----", ind);
+          return <li key={ind}>{val}</li>;
+        })}
+      </h3>
+      <h3>Friend Name: {props.friend.fname}</h3>
+      <h4>Mobile No : {props.friend.mobile}</h4>
+      <h4>Address: {props.friend.address}</h4>
+      <hr />
+    </>
+  );
+};
+
+ChildComponent.defaultProps = {
+  sname: "No Name",
+  age: 18,
+  course: "Generic",
+  skills: [],
+  friend: { fname: "No friends", address: "Not Mentioned", mobile: 0 },
+};
+
+export default ChildComponent;
+*/
+
+// ? Pure JavaScript Data types
+/*
+let ChildComponent = props => {
+  console.log(props);
+
+  let { sname, age, isPlaced, companyName, salary,  skills, address, dob, clickFn } = props;
+
+  return (
+    <>
+      <h2>I am ChildComponent</h2>
+      <h3>Name : {sname}</h3>
+      <h3>Age : {age}</h3>
+      <h3>Placed Status : {isPlaced === true ? "YES" : "NO"}</h3>
+      <h3>Placed Status : {isPlaced ? "YES" : "NO"}</h3>
+      <h3>
+        Company Name :
+        {companyName === undefined ? "Not Mentioned" : companyName}
+      </h3>
+      <h3>Salary : {salary === null ? 0 : salary}</h3>
+      <h3>
+        Skills :
+        {skills.map((val, ind) => {
+          return (
+            <div key={ind}>
+              SK00{ind + 1} - {val}
+            </div>
+          );
+        })}
+      </h3>
+      <h3>Address : </h3>
+      <div>Street No : {address.streetno}</div>
+      <div>Colony : {address.colony}</div>
+      <div>Pincode : {address.pincode}</div>
+      <div>DOB : {dob.toString()}</div>
+      <button onClick={clickFn}>click Here </button>
+    </>
+  );
+};
+export default ChildComponent;
+*/
+
+// ? ==== props drilling ====
+/*
+import GrandChildComponent from "./GrandChildComponent";
+let ChildComponent = props => {
+  console.log(props);
+  console.log("From ChildComponent -", props.products);
+
+  return (
+    <>
+      <h2>I am ChildComponent</h2>
+      <h2>
+        Link : <a href={props.data}>Click Here</a>
+      </h2>
+      <GrandChildComponent d={props} />
+    </>
+  );
+};
+export default ChildComponent;
+*/
+
+// ? ========= prop types ==============
+import PropTypes from "prop-types";
+
+let ChildComponent = props => {
+  console.log(props);
+  let { sname, course, skills } = props;
+  return (
+    <>
+      <h2>I am ChildComponent </h2>
+      <h3>Name : {sname}</h3>
+      <h3>Course : {course}</h3>
+      <h3>
+        Skills :
+        {skills.map((val, ind) => {
+          return <li key={ind}>{val}</li>;
+        })}
+      </h3>
+    </>
+  );
+};
+
+ChildComponent.propTypes = {
+  sname: PropTypes.string,
+  age: PropTypes.number,
+  course: PropTypes.string,
+  skills: PropTypes.array,
+  address: PropTypes.object,
+  isMarried: PropTypes.bool,
+};
+
 export default ChildComponent;
